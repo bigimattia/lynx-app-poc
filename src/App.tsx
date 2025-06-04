fda81f67df257f5e3247a85b7a9be95942158576
Mattia <mattia.bigi@outlook.it>
2025-06-04 10:14:20 +0200

    add tailwind v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,51 @@
-import { useCallback, useEffect, useState } from '@lynx-js/react'
+import { useCallback, useEffect, useState } from "@lynx-js/react";
 
-import './App.css'
-import arrow from './assets/arrow.png'
-import lynxLogo from './assets/lynx-logo.png'
-import reactLynxLogo from './assets/react-logo.png'
+import "./App.css";
+import arrow from "./assets/arrow.png";
+import lynxLogo from "./assets/lynx-logo.png";
+import reactLynxLogo from "./assets/react-logo.png";
 
-export function App(props: {
-  onMounted?: () => void
-}) {
-  const [alterLogo, setAlterLogo] = useState(false)
+export function App(props: { onMounted?: () => void }) {
+  const [alterLogo, setAlterLogo] = useState(false);
 
   useEffect(() => {
-    console.info('Hello, ReactLynx')
-    props.onMounted?.()
-  }, [])
+    console.info("Hello, ReactLynx");
+    props.onMounted?.();
+  }, []);
 
   const onTap = useCallback(() => {
-    'background only'
-    setAlterLogo(!alterLogo)
-  }, [alterLogo])
+    "background only";
+    setAlterLogo(!alterLogo);
+  }, [alterLogo]);
 
   return (
     <view>
-      <view className='Background' />
-      <view className='App'>
-        <view className='Banner'>
-          <view className='Logo' bindtap={onTap}>
-            {alterLogo
-              ? <image src={reactLynxLogo} className='Logo--react' />
-              : <image src={lynxLogo} className='Logo--lynx' />}
+      <view className="Background" />
+      <view className="App">
+        <view className="Banner">
+          <view className="Logo" bindtap={onTap}>
+            {alterLogo ? (
+              <image src={reactLynxLogo} className="Logo--react" />
+            ) : (
+              <image src={lynxLogo} className="Logo--lynx" />
+            )}
           </view>
-          <text className='Title'>React</text>
-          <text className='Subtitle'>on Lynx</text>
+          <text className="Title">React</text>
+          <text className="Subtitle">on Lynx</text>
+          <text className="underline text-3xl border-b-emerald-50 border-b-2">
+            TEST
+          </text>
         </view>
-        <view className='Content'>
-          <image src={arrow} className='Arrow' />
-          <text className='Description'>Tap the logo and have fun!</text>
-          <text className='Hint'>
-            Edit<text style={{ fontStyle: 'italic' }}>{' src/App.tsx '}</text>
+        <view className="Content">
+          <image src={arrow} className="Arrow" />
+          <text className="Description">Tap the logo and have fun!</text>
+          <text className="Hint">
+            Edit<text style={{ fontStyle: "italic" }}>{" src/App.tsx "}</text>
             to see updates!
           </text>
         </view>
         <view style={{ flex: 1 }}></view>
       </view>
     </view>
-  )
+  );
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,6 @@
+import preset from "@lynx-js/tailwind-preset";
+
+export default {
+  presets: [preset],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+};
